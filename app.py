211f967e4e54ead9ed08822992c5a23c9c934b7e
Muhammad Rafay <muhammadrafay7655@gmail.com>
2023-01-11 01:51:10 +0500

    Edit azure pipeline

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Docker!, File has been changed, File has been edited :)'
+    return 'Hello, Docker!, File has been changed'
